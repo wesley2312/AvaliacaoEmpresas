a853e0d114b0ee73016d9c6e966c9555ccb35fb3
wesker <wesker@DESKTOP-KQMLKBQ>
2023-06-01 20:23:34 -0300

    trabalho

---
+++ src/avaliacaoempresas/AvaliacaoEmpresas.java
@@ -0,0 +1,15 @@
+
+package avaliacaoempresas;
+
+import javax.swing.JOptionPane;
+
+public class AvaliacaoEmpresas {
+
+    
+
+    public static void main(String[] args) {
+    System.out.println("digite o seu nome");
+        System.out.println("digite o seu nome");
+    }
+    
+}
